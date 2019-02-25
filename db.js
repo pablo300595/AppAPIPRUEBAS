e8fe66b3715ae8849f51e74b85c750225f0f3896
pablo300595 <jopavalenzuelami@ittepic.edu.mx>
2019-02-24 19:19:58 -0600

    feat: ajustes para version local

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = `mongodb://admin:admin@cluster0-shard-00-00-qkg0z.mongodb.net:27017,cluster0-shard-00-01-qkg0z.mongodb.net:27017,cluster0-shard-00-02-qkg0z.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`;
+const URI = 'mongodb://localhost/test-students';
 
 mongoose.connect(URI,{ useNewUrlParser: true })
     .then(db => console.log('db is connected'))
