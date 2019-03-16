554d215cba1f856cccd449d55a1d0e59f298be74
Francisco <fco-javier123@hotmail.com>
2019-03-16 11:04:11 -0700

    feat: Ultimos cambios

--- models/alumno.model.js
+++ models/alumno.model.js
@@ -28,7 +28,9 @@ const AlumnoSchema=new Schema({
     nameSchool: {type:String, required:true},
     average: {type:Number, required:true},
     career: {type:String, required:true},
-    documents: {type: [String], required:false},
+    documents: {type: [
+        {documento: String, status:String, observacion:String}
+    ], required:false},
     statusInscripcion: {type: String, required:false},
     validated:{type: Boolean, required:false}
 });
