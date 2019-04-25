36e501a06b81ab27eb047f688b43d22023281244
pablo300595 <jopavalenzuelami@ittepic.edu.mx>
2019-04-25 13:00:52 -0500

    fix: Agregar documento al movimiento

--- models/secretaria-movement.model.js
+++ models/secretaria-movement.model.js
@@ -5,6 +5,7 @@ const SecretariaMovementSchema = new Schema({
     usuario: {type: Schema.ObjectId, ref: 'Usuario'},
     secretaria: {type: Schema.ObjectId, ref: 'Secretaria'},
     alumno: {type: Schema.ObjectId, ref: 'Alumno'},
+    document: {type: String, required: true},
     action: {type:String, required:true},
     dateModificationServer: {type: Date, required: true},
     dateModificationClient: {type: Date, required: true},
