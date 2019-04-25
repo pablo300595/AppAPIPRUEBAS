dfb13148debea8f7ab6765dc07775956c6813d24
pablo300595 <jopavalenzuelami@ittepic.edu.mx>
2019-04-25 12:20:56 -0500

    feat: referencia alumno en movimientos de secretaria

--- models/secretaria-movement.model.js
+++ models/secretaria-movement.model.js
@@ -4,6 +4,7 @@ const {Schema} = mongoose;
 const SecretariaMovementSchema = new Schema({
     usuario: {type: Schema.ObjectId, ref: 'Usuario'},
     secretaria: {type: Schema.ObjectId, ref: 'Secretaria'},
+    alumno: {type: Schema.ObjectId, ref: 'Alumno'},
     action: {type:String, required:true},
     dateModificationServer: {type: Date, required: true},
     dateModificationClient: {type: Date, required: true},
